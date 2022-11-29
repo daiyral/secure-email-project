5b0f611699ba5afc0c07455af01920c3ba62abbf
Danny Aibinder <DannyAibinder@gmail.com>
2022-11-29 19:30:38 +0200

    added functions

--- frog.py
+++ frog.py
@@ -1,10 +1,64 @@
 #frog encryption decryption
 import numpy as np
 
-def frogEncrypt():
+BLOCK_SIZE = 16
+
+NUM_ITERATIONS = 8
+
+class FrogIterKey:
+    def __init__(self):
+        self.xorBu =  np.empty(BLOCK_SIZE, dtype = np.int32)
+        self.SubstPermu = np.empty(256,dtype=np.int32)
+        self.BombPermu = np.empty(BLOCK_SIZE,dtype=np.int32)
+    
+    def size():
+        return BLOCK_SIZE*2+256
+
+    def setValue(self,i, value):
+        if value < 0:
+            value = 256  + value
+        if i < BLOCK_SIZE:
+            self.xorBu[i] = value
+        elif i < BLOCK_SIZE + 256:
+            self.SubstPermu[i-BLOCK_SIZE] = value
+        else:
+            self.BombPermu[i-BLOCK_SIZE-256] = value
+    
+    def getValue(self,i):
+        if i < BLOCK_SIZE:
+            return self.xorBu[i]
+        elif i < BLOCK_SIZE + 256:
+            return self.SubstPermu[i-BLOCK_SIZE]
+        else:
+            return self.BombPermu[i-BLOCK_SIZE-256]
+
+    def copyFrom(self,origin):
+        self.xorBu = origin.xorBu
+        self.SubstPermu = origin.SubstPermu
+        self.BombPermu = origin.BombPermu
+
+
+
+def frogEncrypt(plainText, key):
+    #Encrypt plainText using internalKey - (internal cycle) See B.1.1
+    for i in range (0, NUM_ITERATIONS):
+        for j in range (0, BLOCK_SIZE):
+            plainText[j] = plainText[j] ^ key[i].xorBu[j]
+            if plainText[j]<0:
+                plainText[j] = key[i].SubstPermu[plainText[j]+256]
+            else:
+                plainText[j] = key[i].SubstPermu[plainText[j]]
+            if j< BLOCK_SIZE -1:
+                plainText[j+1]= plainText[j+1] ^ plainText[j]
+            else:
+                plainText[0] = plainText[0] ^ plainText[BLOCK_SIZE-1]
+            plainText[key[i].bombPermu[j]] ^= plainText[j] 
+    return plainText
+    
+def frogDecrypt():
     pass
 
-def frogDecrypt():
+def createInternalKey(key):
     pass
 
 def makePermutation( permu):
