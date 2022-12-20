11312b5369551ba8d3f05e20dbd4ddc9541d1c40
matan034 <matan034@gmail.com>
2022-12-20 17:43:39 +0200

    combining merkle hellman
    Co-authored-by: Daniel Aibinder <daiyral@users.noreply.github.com>

--- gui.py
+++ gui.py
@@ -34,6 +34,10 @@ class Gui(QMainWindow):
         self.frog = Frog()
         self.merkle_helman = MerkleHellman()
         self.el_gamal_signature = ElGamal()
+        # k = np.empty(16, dtype=np.int8)
+        # for i in range(0, 16):
+        #     k[i] = i
+        self.intKey=self.frog.makeKey(self.merkle_helman.get_public_key())
         self.set_encrypt_section()
         self.set_decrypt_section()
         window = QWidget()
@@ -49,8 +53,7 @@ class Gui(QMainWindow):
         self.email_input.setPlaceholderText("Enter your e-mail")
         self.email_enc_btn = QPushButton("Encrypt")
         self.email_enc_btn.clicked.connect(self.encrypt_msg)
-        self.public_params_lbl = QLabel("Diffie-Hellman Key Public Params: public key={pub_key}, private key={private_key}".format(pub_key=self.merkle_helman.get_public_key(),
-                                        private_key=self.merkle_helman.get_private_key()))
+        self.public_params_lbl = QLabel("Diffie-Hellman Key Public Params: public key={pub_key}, private key={private_key}".format(pub_key="None", private_key="None"))
         self.public_params_lbl.setFont(self.font)
         self.email_encrypted = QTextEdit()
         self.email_encrypted.setFont(self.font)
@@ -65,6 +68,7 @@ class Gui(QMainWindow):
     
     def set_decrypt_section(self):
         self.email_dec_btn = QPushButton("Decrypt")
+        self.email_dec_btn.clicked.connect(self.decrypt_msg)
         self.email_decrypted = QTextEdit()
         self.email_decrypted.setFont(self.font)
         self.email_decrypted.setPlaceholderText("Decrypted email")
@@ -74,20 +78,26 @@ class Gui(QMainWindow):
         
     def encrypt_msg(self):
         msg = self.email_input.toPlainText()
-        arr = bytes(msg, 'utf-8')
-        print(arr)
-        k = np.empty(16, dtype=np.int8)
-        for i in range(0, 16):
-            k[i] = i
-        pt=np.empty(paramaters.BLOCK_SIZE, dtype=np.int8)
-        for i in range(0, paramaters.BLOCK_SIZE):
-            pt[i] = i
-        intKey=self.frog.makeKey(k)
-        print("my text is ", pt)
-        cipherText=self.frog.frogEncrypt(pt, intKey.keyE)
-        print("my encrypted text is ",cipherText)
+        msg = bytes(msg, 'utf-8')
+        arr = np.empty(len(msg), dtype=np.int8)
+        i = 0
+        for ch in msg:
+            arr[i] = ch
+            i += 1
+        
+        print("my text is ", arr)
+        self.cipherText=self.frog.frogEncrypt(arr, self.intKey.keyE)
+        print("my encrypted text is ",self.cipherText)
         self.email_encrypted.setText("cipherText")
-
+        
+    def decrypt_msg(self):
+        plainText=self.frog.frogDecrypt(self.cipherText, self.intKey.keyD)
+        string = ""
+        for i in range(len(plainText)):
+            string += chr(plainText[i]).encode("utf-8").decode()
+        print("decrypted: ", plainText)
+        print("msg was = ", string)
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     gui = Gui()
--- merklehellman.py
+++ merklehellman.py
@@ -1,7 +1,38 @@
+import math
+import numpy as np
+import random
+import paramaters
 class MerkleHellman:
     def __init__(self) -> None:
         pass
+    
+    def sort(self, arr):
+        for i in range(len(arr)):
+            for j in range(i+1,len(arr)):
+                if arr[i]>arr[j]:
+                    temp=arr[i]
+                    arr[i]=arr[j]
+                    arr[j]=temp
+        return arr
+
+
+    def sum(self, a):
+        sum=0
+        for i in range(len(a)):
+            sum=sum+a[i]
+        return sum
+    
     def get_public_key(self):
-        pass
-    def get_private_key(self):
-        pass    
+        temp_sum = 0
+        w = np.empty(paramaters.BLOCK_SIZE, dtype=np.int8)
+        b = np.empty(paramaters.BLOCK_SIZE, dtype=np.int8)
+        for i in range(len(w)):
+            temp_sum += i + 1
+            w[i] = temp_sum
+        q = random.randint(temp_sum, 255)
+        r = random.randint(1, 255)
+        while math.gcd(r,q) != 1:
+            r = random.randint(1, 255)
+        for i in range(len(b)):
+            b[i] = r*w[i] % q
+        return b
